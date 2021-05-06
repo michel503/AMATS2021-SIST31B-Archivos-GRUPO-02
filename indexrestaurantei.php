049714da9d91219634887eeac0310b4f48abc85e
MarcoPalma20 <marco.palma20@itca.edu.sv>
2021-05-05 21:10:24 -0600

    se agrago texto al manual y un titulo al index

---
+++ indexrestaurantei.php
@@ -0,0 +1,13 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>pagina principal</title>
+  </head>
+  <body>
+  <h1> BIENVENIDOS al restaurante xxx </h1>
+
+  </body>
+</html>';
+ ?>
